3bc483f7cfdb0b26c7b80612d9b08a55ea03ed52
Guillaume Lanquepin <guillaume.lanquepin@himolde.no>
2009-04-24 17:35:08 +0200

    creation du contructeur

--- heuristic.py
+++ heuristic.py
@@ -1,32 +1,51 @@
 #!/usr/bin/env python
-#       
+#
 #       Copyright 2009 Guillaume Lanquepin <guillaume@himolde.no>
 #		version 0.1
 
 import numpy as np
+from tools import importData
 
 class ClspHeuristic:
-	
-	#_alpha,_beta,_prod,_stor,_cost,_constraint,_item
-	def __init__(self,_dataFolder="data_test.csv",_eps=0.1,_cycle=500):
+
+	def __init__(self,dataFile="data_test.csv",_eps=0.1,_cycle=500):
 		#problem paramters
-		self.T = _T 
-		self.J = _J
+		self.T,\ # time period
+		self.J,\ # number of products
+		self.alpha,\ # demand function parameter
+		self.beta,\ # demand function parameter
+		self.costSetup,\ # setup cost
+		self.costStor,\ # storage cost
+		self.costProd,\ # production cost
+		self.consProd,\ # product consumption
+		self.constraint = importData(dataFile) # production constraint
 		#problem variables
-		coef = np.zeros
+		self.coef = np.zeros(self.T) # lagrangian multiplier
+		self.production = np.zeros((self.J,self.T))
+		self.price = np.zeros((self.J,self.T))
+		self.setup = np.zeros((self.J,self.T))
+		self.storage = np.zeros((self.J,self.T))
 		#algorithm parameters
 		self.eps = _eps #min difference between upper and lower bound (stop condition)
 		self.cycle = _cycle #max number of iteration (stop condition)
 		slef.optUpper = np.zeros(self.T) #vector fo optimun return by Upper
 		slef.optLower = np.zeros(self.T) #vector fo optimun return by Lower
-		
 
-	def Upper(self):
-		
-	def Lower(self):
-		
-	def main(self):
-		
+	def __verbose(self,start):
+		if start:
+			print "### Data"
+			print "Period:"+self.T+"\tProduct:"+self.J
+			print "Alpha:"
+			print self.alpha
+			print "Beta:"
+			print self.beta
+			print "Setup Cost:"
+			print self.costS
+
+	def __Upper(self):
 		return 0
+	def __Lower(self):
+		return 0
+	def main(self):
 
-if __name__ == '__main__': main()
+		return 0
