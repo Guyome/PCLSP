2d9678e2b17569face8c0419e22e8939ab44f08c
Guillaume Lanquepin <guillaume.lanquepin@himolde.no>
2009-04-24 17:35:51 +0200

    correction et test de la fonction importData

--- tools.py
+++ tools.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
-#       
-#       Copyright 2009 Guillaume Lanquepin <guillaume@himolde.no>
+#
+#		Copyright 2009 Guillaume Lanquepin <guillaume@himolde.no>
 #		version 0.1
+#
+# This file containts all tools needed by heuristic program
 import csv
+import numpy as np
 
-def importData(adress):
+def importData(address):
 		try:
 			input= open(address, "r")
 			inputCsv=csv.reader(input,delimiter=";",lineterminator="\n")
 		except IOError:
 			print "I/O error with the specified file"
-			
-		data = list()
-		item = list()
+
+		data = list() # all data
+		item = list() # each tabular
 		count = 0
 		for row in inputCsv:
-			if count < 2 :
-				data.append(row[1])
-				count =+ 1
-			else
-				item.append(row[1:]])
-				if ((count-2)%data[2] == 0):
-					data.append(item)
+			if count < 2 : # read Time period and number of product
+				data.append(int(row[1]))
+			else :
+				item.append(row[1:])
+				if ((count-2)%data[1] == 1):
+					data.append(np.array(item,dtype=float))
 					item = list()
-				
+			count += 1
+		data.append(np.array(item,dtype=float)) # manage the last tabular
+		return data
+
+if __name__ == '__main__':
+	test = importData("data_test.csv")
+	print test
