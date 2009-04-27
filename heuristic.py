cabeb9eabbfdfe879ed6c9a67652fe1f2b798ef7
Guillaume Lanquepin <guillaume.lanquepin@himolde.no>
2009-04-27 10:35:16 +0200

    add verbose fonction

--- heuristic.py
+++ heuristic.py
@@ -4,21 +4,21 @@
 #       version 0.1
 
 import numpy as np
-from tools import importData
+import tools as tl
 
 class ClspHeuristic:
 
-    def __init__(self,dataFile="data_test.csv",_eps=0.1,_cycle=500):
+    def __init__(self,dataFile="data_test.csv", _eps=0.1, _cycle=500,verbose=True):
         #problem paramters
-        self.T,\ # time period
-        self.J,\ # number of products
-        self.alpha,\ # demand function parameter
-        self.beta,\ # demand function parameter
-        self.costSetup,\ # setup cost
-        self.costStor,\ # storage cost
-        self.costProd,\ # production cost
-        self.consProd,\ # product consumption
-        self.constraint = importData(dataFile) # production constraint
+        self.T, \
+        self.J, \
+        self.alpha, \
+        self.beta, \
+        self.costSetup, \
+        self.costStor, \
+        self.costProd, \
+        self.consProd, \
+        self.constraint = tl.importData(dataFile)
         #problem variables
         self.coef = np.zeros(self.T) # lagrangian multiplier
         self.production = np.zeros((self.J,self.T))
@@ -28,25 +28,38 @@ class ClspHeuristic:
         #algorithm parameters
         self.eps = _eps #min difference between upper and lower bound (stop condition)
         self.cycle = _cycle #max number of iteration (stop condition)
-        slef.optUpper = np.zeros(self.T) #vector fo optimun return by Upper
-        slef.optLower = np.zeros(self.T) #vector fo optimun return by Lower
+        self.optUpper = np.zeros(self.T) #vector fo optimun return by Upper
+        self.optLower = np.zeros(self.T) #vector fo optimun return by Lower
+        if verbose:
+            self.__verbose (True)
 
     def __verbose(self,start):
         if start:
             print "### Data"
-            print "Period:"+self.T+"\tProduct:"+self.J
-            print "Alpha:"
-            print self.alpha
-            print "Beta:"
-            print self.beta
-            print "Setup Cost:"
-            print self.costSetup
-            print
+            print "Period:"+str(self.T)+"\tProduct:"+str(self.J)
+            print "Alpha:",
+            tl.printtab(self.alpha)
+            print "Beta:",
+            tl.printtab(self.beta)
+            print "Setup Cost:",
+            tl.printtab(self.costSetup)
+            print "Storage cost:",
+            tl.printtab(self.costStor)
+            print "Prodcution cost:",
+            tl.printtab(self.costProd)
+            print "Product consumption:",
+            tl.printtab(self.constraint)
+            print "Production constraint",
+            tl.printtab(self.constraint)
 
-    def __Upper(self):
+    def __upper(self):
         return 0
-    def __Lower(self):
+    def __lower(self):
         return 0
     def main(self):
 
         return 0
+
+if __name__ == '__main__':
+    test = ClspHeuristic()
+
