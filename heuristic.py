e65d8d9336d4f04f634c840a87850faef7e74e89
Guillaume Lanquepin <guillaume.lanquepin@himolde.no>
2009-04-30 12:07:44 +0200

    Change name variable and import lupb.py

--- heuristic.py
+++ heuristic.py
@@ -5,40 +5,43 @@
 #       version 0.1
 
 import numpy as np
+from lupb import thomas
 from tools import import_data
 from plots import show_data
 
-class ClspHeuristic:
+class clspHeuristic:
 
-    def __init__(self,dataFile="data_test.csv", _eps=0.1, _cycle=500,verbose=True):
+    def __init__(self, data_file="data_test.csv", _eps=0.1, _cycle=500,verbose=True):
         #problem paramters
-        self.t, \
-        self.J, \
+        self.time_hor, \
+        self.nb_obj, \
         self.alpha, \
         self.beta, \
-        self.costSetup, \
-        self.costStor, \
-        self.costProd, \
-        self.consProd, \
-        self.constraint = import_data(dataFile)
+        self.cost_setup, \
+        self.cost_stor, \
+        self.cost_prod, \
+        self.cons_prod, \
+        self.constraint = import_data(data_file)
         #problem variables
-        self.coef = np.zeros(self.t) # lagrangian multiplier
-        self.production = np.zeros((self.J,self.t))
-        self.price = np.zeros((self.J,self.t))
-        self.setup = np.zeros((self.J,self.t))
-        self.storage = np.zeros((self.J,self.t))
+        self.coef = np.zeros(self.time_hor,float) # lagrangian multiplier
+        self.production = np.zeros((self.nb_obj, self.time_hor),float)
+        self.price = np.zeros((self.nb_obj, self.time_hor),float)
+        self.setup = np.zeros((self.nb_obj, self.time_hor),float)
+        self.storage = np.zeros((self.nb_obj, self.time_hor),float)
         #algorithm parameters
         self.eps = _eps #min difference between upper and lower bound (stop condition)
         self.cycle = _cycle #max number of iteration (stop condition)
-        self.optUpper = np.zeros(self.t) #vector fo optimun return by Upper
-        self.optLower = np.zeros(self.t) #vector fo optimun return by Lower
+        self.opt_upper = np.zeros(self.time_hor) #vector fo optimun return by Upper
+        self.opt_lower = np.zeros(self.time_hor) #vector fo optimun return by Lower
         if verbose:
             show_data(self)
 
     def main(self):
-
+        upper,delta = thomas(self)
+        print upper,delta
         return 0
 
 if __name__ == '__main__':
-    TEST = ClspHeuristic()
+    TEST = clspHeuristic()
+    TEST.main()
 
