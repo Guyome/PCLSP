9b03b16d49f920a49431d1d42d8cea3c38db8123
Guillaume Lanquepin <guillaume.lanquepin@himolde.no>
2009-04-30 12:06:41 +0200

    move print_tab to plots.py and modify clsp name variable

--- plots.py
+++ plots.py
@@ -3,22 +3,30 @@
 #
 #       Copyright 2009 Guillaume Lanquepin <guillaume@himolde.no>
 #       version 0.1
-from tools import *
+
+def print_tab(tabular):
+    row,col = tabular.shape
+    tab = ""
+    for i in xrange(row):
+        tab += "\n\t|"
+        for j in xrange(col):
+            tab += str(tabular[i][j])+"|"
+    print tab
 
 def show_data(clsp):
     print "### Data"
-    print "Period:"+str(clsp.t)+"\tProduct:"+str(clsp.J)
+    print "Period:"+str(clsp.time_hor)+"\tProduct:"+str(clsp.nb_obj)
     print "Alpha:",
     print_tab(clsp.alpha)
     print "Beta:",
     print_tab(clsp.beta)
     print "Setup Cost:",
-    print_tab(clsp.costSetup)
+    print_tab(clsp.cost_setup)
     print "Storage cost:",
-    print_tab(clsp.costStor)
+    print_tab(clsp.cost_stor)
     print "Prodcution cost:",
-    print_tab(clsp.costProd)
+    print_tab(clsp.cost_prod)
     print "Product consumption:",
-    print_tab(clsp.constraint)
+    print_tab(clsp.cons_prod)
     print "Production constraint",
     print_tab(clsp.constraint)
