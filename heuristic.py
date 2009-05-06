f05eb0208fef50ea387ca12cf0c1c92104472d54
Guillaume Lanquepin <guyomel@gmail.com>
2009-05-06 17:39:52 +0200

    Write heuristic

--- heuristic.py
+++ heuristic.py
@@ -6,13 +6,15 @@
 
 import numpy as np
 from lupb import thomas
-from llpb import ipopt
+from llpb import ipopt, heuristic
 from tools import import_data
 from plots import show_data
 
-class clspHeuristic:
+class PCLSP:
+    """
+    """
 
-    def __init__(self, data_file="data_test.csv", _eps=0.1, _cycle=500,verbose=True):
+    def __init__(self, data_file="data_test.csv", theta=0.3, eps=0.1, cycle=500, verbose=True):
         #problem paramters
         self.time_hor, \
         self.nb_obj, \
@@ -24,26 +26,78 @@ class clspHeuristic:
         self.cons_prod, \
         self.constraint = import_data(data_file)
         #problem variables
-        self.coef = np.zeros(self.time_hor,float) # lagrangian multiplier
-        self.production = np.zeros((self.nb_obj, self.time_hor),float)
-        self.price = np.zeros((self.nb_obj, self.time_hor),float)
-        self.setup = np.zeros((self.nb_obj, self.time_hor),float)
-        self.storage = np.zeros((self.nb_obj, self.time_hor),float)
+        self.coef = np.zeros(((self.nb_obj+1)*self.time_hor), float) # lagrangian multiplier
+        self.production = np.zeros((self.nb_obj, self.time_hor), float)
+        self.price = np.zeros((self.nb_obj, self.time_hor), float)
+        self.setup = np.zeros((self.nb_obj, self.time_hor), float)
+        self.storage = np.zeros((self.nb_obj, self.time_hor), float)
         #algorithm parameters
-        self.eps = _eps #min difference between upper and lower bound (stop condition)
-        self.cycle = _cycle #max number of iteration (stop condition)
-        self.opt_upper = np.zeros(self.time_hor) #vector fo optimun return by Upper
-        self.opt_lower = np.zeros(self.time_hor) #vector fo optimun return by Lower
-        if verbose:
+        self.eps = eps #min difference between upper and lower bound
+        self.cycle = cycle #max number of iteration (stop condition)
+        self.smooth_param = theta
+        self.verbose = verbose
+        self.opt_upper = []#vector fo optimun return by Upper
+        self.opt_lower = []#vector fo optimun return by Lower
+        if self.verbose:
             show_data(self)
 
-    def main(self):
-        upper,delta = thomas(self)
-        ipopt(self)
-        print upper,delta
-        return 0
+        
+    def _critere(self):
+        """
+        
+        """
+        objective = 0
+        for j in xrange(self.nb_obj):
+            for t in xrange(self.time_hor):
+                objective += (self.alpha[j,t]-self.beta[j,t]*self.price[j,t])*self.price[j,t]\
+                - self.cost_setup[j,t]*self.setup[j,t] \
+                - self.cost_stor[j,t]*self.storage[j,t] \
+                - self.cost_prod[j,t]*self.production[j,t]
+        return objective
+        
+    def _critere_ipopt(self,initial):
+        """
+            
+        """
+        objective = initial
+        for j in xrange(self.nb_obj):
+            for t in xrange(self.time_hor):
+                objective -= self.cost_setup[j,t]*self.setup[j,t]
+        return objective
+        
+    def solve(self):
+        """
+        
+        """
+        diff = self.eps+ 1.
+        count = 0
+        while ( (diff > self.eps) & (count < self.cycle) ):
+            previous_lambda = self.coef
+            self.price, self.setup = thomas(self)
+            upper = self._critere()
+            #try:
+            lower, self.coef = ipopt(self)
+            lower = self._critere_ipopt(lower)
+            #except:
+            #    print "Ipopt solver unaviable: run heuristic..."
+            #    self.price, self.coef = heuristic(self)
+            #    lower = self._critere()
+            self.opt_upper.append(upper)
+            self.opt_lower.append(lower)
+            # update lagrangian coefficients
+            for i in xrange(len(self.coef)):
+                self.coef[i] = self.smooth_param*previous_lambda[i]\
+                - (1-self.smooth_param)*self.coef[i] 
+            diff = upper-lower
+            count += 1
+        if self.verbose:
+            print "Number of iteration: "+str(count)
+            print "Difference between upper and lower bound: "+str(diff)
+            print "Last critere value: "+str(lower)
+            show_data(self)
+        return self._critere ()
 
 if __name__ == '__main__':
-    TEST = clspHeuristic()
-    TEST.main()
+    TEST = PCLSP()
+    TEST.solve()
 
