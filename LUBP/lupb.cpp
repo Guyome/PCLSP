632d600ee8513150077f38076ffab7d35d30d2a5
Guillaume Lanquepin <guyomel@gmail.com>
2009-05-07 17:22:03 +0200

    manage verbosity + some corrections

--- LUBP/lupb.cpp
+++ LUBP/lupb.cpp
@@ -3,84 +3,71 @@
 
 using namespace blitz;
 
-int ipopt(Array<double,2> alpha,Array<double,2> beta, Array<double,2> prod,
-            Array<double,2> stor, Array<double,2> consumption,
-            Array<double,1> lambda, blitz::Array<double,1> results,
-            int period, int product,int verbose)
+int thomas(Array<double,2> alpha,Array<double,2> beta, Array<double,2> prod,
+            Array<double,2> stor, Array<double,2> cons, Array<double,2> setup,
+             Array<double,2> results, Array<double,1> lambda,
+            int hor, int obj, int verbose)
 {
     //problem variable
     Array<double,1> c(hor);
-    Array<double,1> f(hor);
+    Array<double,1> f(hor+1);
     Array<double,2> price(hor,hor);
     Array<double,2> demand(hor,hor);
     // algorithm variable
-    Array<int,1> ind;
-    int k,i,t0,t = 0,;
-    float sumstor,sum,summin;
-    
-    //initiate price,demand since there is no storage
+    Array<int,1> ind(hor);
+    float sumc; //summin;
+    int t;
+    if (verbose >0)
+    {
+        printf("j\tt\tF(t)\t\tInd\n");
+        printf("-------------------------------------\n");
+    }
     for(int j = 0; j < obj;  j++)
     {
-        price(t,t) = (alpha(j,t) + (prod(j,t) + cons(j,t)*lamdba(j,t))* beta(j,t)) / (2 * beta(j,t) );
+        //initiate price,demand since there is no storage
+        t = 0;
+        price(t,t) = (alpha(j,t) + (prod(j,t) + cons(j,t)*lambda(j,t))* beta(j,t)) / (2 * beta(j,t) );
         demand(t,t) = alpha(j,t) - beta(j,t) * price(t,t);
         c(t)= demand(t,t)*( price(t,t) - prod(j,t))-setup(j,t);
         f(t+1) = -c(t);
+        // result for the first period
         ind(0) = 0;
-        for ( t = 1; t < hor;  t++)
+        results(j,t)=price(t,ind(t));
+        for (t = 1; t < hor;  t++)
         {
-            for (t0 = 0; t0 <= t; t0++)
+            for (int t0 = 0; t0 <= t; t0++)
             {
-                // sum of first storage
-                sumstor = 0;
-                for (i = t0; i < t; i++)
-                {
-                    sumstor += stor(j,i);
-                }
-                // compute price and demand
-                price(t,t0) = (alpha(j,t) + 
-                    (prod(j,t0) + sumstor + cons(j,t0)*lamdba(j,t0))* beta(j,t))
-                     / (2 * beta(j,t));
+                // compute price
+                price(t,t0) = (alpha(j,t) + (prod(j,t0) + sum(stor(j,Range(t0,t-1)))
+                    + cons(j,t0)*lambda(j,t0))* beta(j,t)) / (2 * beta(j,t));
+                // compute demand
                 demand(t,t0) = alpha(j,t) - beta(j,t) * price(t,t0);
-            }
-            for (t0 = 0; t0 <= t; t0++)
-            {
-                sum = 0;
-                for (i = t0; i <= t; i++)
+                // compute cost function
+                sumc = 0;
+                for (int i = t0; i <= t; i++)
                 {
-                    sumstor = 0;
-                    for (k = t0; k < i; k++)
-                    {
-                        sumstor += stor(j,i);
-                    }
-                    sum += (prod(j,t0) + sumstor - price(j,t0))*demand(j,t0);
+                    sumc += (prod(j,t0) + sum(stor(j,Range(t0,i-1))) - price(i,t0))*demand(i,t0);
                 }
-                c(t0) = sum + setup(j,t0);
+                c(t0) = sumc + setup(j,t0);
             }
-            // compute minimal criterium
-            summin = 0;
-            for (t0 = 0; t0 <= t; t0++)
-            {
-                if (c(t0) + f(t0) < summin)
-                {
-                    summin = c(t0) + f(t0);
-                    ind(t) = t0;
-                }
-            }
-            f(t+1) = summin;
-        }
-        for( t = 0; t < hor;  t++)
-        {
-            optiprice(j,t)=price(t,ind(t));
+            // find minimal criterium
+            f(t+1) = min(c(Range(0,t))+ f(Range(0,t)));
+            // result for period t
+            ind(t) = min(minIndex(c(Range(0,t))+ f(Range(0,t))));
+            results(j,t)=price(t,ind(t));
         }
         
-        printf("t \t F(t) \t Ind\n");
-        printf("--------------------------------\n");
-        for (t = 0; t < hor; t++)
+        if (verbose >0)
         {
-            printf("%d\t%f\t%d\n",t+1,-f(t+1),ind(t)+1);
+            for (t = 0; t < hor; t++)
+            {
+                printf("%d\t%d\t%f\t%d\n",j+1,t+1,-f(t+1),ind(t)+1);
+            }
         }
-        printf("--------------------------------\n");
+    }
+    if (verbose>0)
+    {
+        printf("-------------------------------------\n");
     }
     return 0;
 }
-
