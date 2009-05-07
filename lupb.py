88fb0fa02363d67c17bacdc8401c2afd2ec28fba
Guillaume Lanquepin <guyomel@gmail.com>
2009-05-07 17:26:14 +0200

    import data from function

--- lupb.py
+++ lupb.py
@@ -7,84 +7,32 @@
 import scipy.weave as wv
 from scipy.weave import converters
 import numpy as np
+from os.path import join, split
 
-def thomas(clsp):
-    
-    hor = clsp.time_hor
-    obj = clsp.nb_obj
-    alpha  = clsp.alpha
-    beta = clsp.beta
-    setup = clsp.cost_setup
-    prod = clsp.cost_prod
-    stor = clsp.cost_stor
-    lamdba = clsp.coef
-    cons = clsp.cons_prod
-    optiprice = np.zeros((obj,hor), float)
-    thomas_code = """
-        //problem variable
-        float c[hor];
-        float f[hor];
-        float price[hor][hor];
-        float demand[hor][hor];
-        // algorithm variable
-        int k,i,t0,t = 0,ind[hor];
-        float sumstor,sum,summin;
-        
-        //initiate price,demand since there is no storage
-        for(int j = 0; j < obj;  j++)
-        {
-            price[t][t] = (alpha(j,t) + (prod(j,t) + cons(j,t)*lamdba(j,t))* beta(j,t)) / (2 * beta(j,t) );
-            demand[t][t] = alpha(j,t) - beta(j,t) * price[t][t];
-            c[t]= demand[t][t]*( price[t][t] - prod(j,t))-setup(j,t);
-            f[t+1] = -c[t];
-            ind[0] = 0;
-            for ( t = 1; t < hor;  t++)
-            {
-                for (t0 = 0; t0 <= t; t0++)
-                {
-                    // sum of first storage
-                    sumstor = 0;
-                    for (i = t0; i < t; i++)
-                    {
-                        sumstor += stor(j,i);
-                    }
-                    // compute price and demand
-                    price[t][t0] = (alpha(j,t) + 
-                        (prod(j,t0) + sumstor + cons(j,t0)*lamdba(j,t0))* beta(j,t)) / (2 * beta(j,t));
-                    demand[t][t0] = alpha(j,t) - beta(j,t) * price[t][t0];
-                }
-                for (t0 = 0; t0 <= t; t0++)
-                {
-                    sum = 0;
-                    for (i = t0; i <= t; i++)
-                    {
-                        sumstor = 0;
-                        for (k = t0; k < i; k++)
-                        {
-                            sumstor += stor(j,i);
-                        }
-                        sum += (prod(j,t0) + sumstor - price[j][t0])*demand[j][t0];
-                    }
-                    c[t0] = sum + setup(j,t0);
-                }
-                // compute minimal criterium
-                summin = 0;
-                for (t0 = 0; t0 <= t; t0++)
-                {
-                    if (c[t0] + f[t0] < summin)
-                    {
-                        summin = c[t0] + f[t0];
-                        ind[t] = t0;
-                    }
-                }
-                f[t+1] = summin;
-            }
-            for( t = 0; t < hor;  t++)
-            {
-                optiprice(j,t)=price[t][ind[t]];
-            }
-        }
+def thomas(alpha, beta, cost_prod,
+    cost_stor, cons_prod, cost_setup,
+    opti_price, coef, time_hor, nb_obj, verbose):
     """
-    wv.inline( thomas_code , ['hor', 'obj','alpha', 'beta', 'setup', 'prod', 'stor', 'optiprice','cons','lamdba'], \
-    type_converters=converters.blitz)
-    return optiprice, np.array(optiprice > 0, int)
+    This function is an adapted version of J. Thomas's algorithm
+    (see price-production decision with deterministic demand 1970)
+    for lagrangian relaxatio and multi product 
+    """
+    opti_price = np.zeros((clsp.nb_obj,clsp.time_hor), float)
+    extra_code = open(join(split(__file__)[0],'LUBP/lupb.cpp')).read()
+    thomas_code = """
+    int status = ipopt(alpha, beta, cost_prod,
+        cost_stor, cons_prod, cost_setup,
+        opti_price, coef,
+        time_hor, nb_obj, 1);
+    """
+    wv.inline( thomas_code ,\
+    ['time_hor', 'nb_obj','alpha', 'beta', 'cost_setup',\
+    'cost_prod', 'cost_stor', 'opti_price','cons_prod','coef','verbose'], \
+    support_code=extra_code,\
+    type_converters=converters.blitz,verbose=2)
+    return opti_price, np.array(opti_price > 0, int)
+    
+if __name__ == '__main__':
+    from heuristic import *
+    TEST = PCLSP()
+    print thomas(TEST)
