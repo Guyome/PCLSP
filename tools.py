1726839a50dd5b82146c52591db59cc0663f7de6
Guillaume Lanquepin <guyomel@gmail.com>
2009-05-08 15:57:30 +0200

    add docstring, correct import_data

--- tools.py
+++ tools.py
@@ -9,6 +9,26 @@ import csv
 import numpy as np
 
 def import_data(address):
+    """
+    This function import data form cvs file define as 
+    in the following example:
+    
+    "T";4
+    "J";2
+    "Alpha";100;100;100;100
+    ;100;100;100;100
+    "Beta";1;1;1;1
+    ;1;1;1;1
+    "Setup cost";3;3;3;3
+    ;3;3;3;3
+    "Storage cost";2;2;2;2
+    ;2;2;2;2
+    "Production cost";20;20;20;20
+    ;2;2;2;2
+    "Consuption";0.3;0.3;0.3;0.3
+    ;2;2;2;2
+    "Constraint";35;40;42;24
+    """
     try:
         inputcsv = csv.reader(open(address, "r"), delimiter=";", lineterminator="\n")
     except IOError:
@@ -17,16 +37,19 @@ def import_data(address):
     data = list() # all data
     item = list() # each tabular
     count = 0
+    subcount = 0
     for row in inputcsv:
         if count < 2 : # read Time period and number of product
             data.append(int(row[1]))
         else :
             item.append(row[1:])
-            if (((count-2)%data[1] == 1)|(data[1] == 1)):
+            subcount +=1 
+            if subcount == data[1]:
                 data.append(np.array(item, dtype=float))
                 item = list()
+                subcount = 0
         count += 1
-    if (data[1] != 1):
+    if (data[1] > 1):
         data.append(np.array(item, dtype=float)) # manage the last tabular
     return data
 
